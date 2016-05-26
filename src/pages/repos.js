7d99b563c813943c191fcdd21f66b43c5bf5e536
mind_games <dev@mindgames.ru>
2016-05-26 09:32:14 +0200

    lesson-8

---
+++ src/pages/repos.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default React.createClass({
+    render (){
+        return (
+            <div className='container'>
+                <header role='banner'>
+                    <h1>Repos</h1>
+                </header>
+            </div>
+        )
+    }
+})
